4ce26cac8d3cfc2c3f31f1beb58371d2797bd62e
unknown <admanoj1@gmail.com>
2021-06-22 21:43:52 +0545

    final project

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-$('.slider').slick{{
-
-}};
+$('.slider').slick({ 
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots'
+});
 
     
-}};
+});
